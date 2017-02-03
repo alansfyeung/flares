6388994ea07170c0900eb7c7c68534e399498482
Alan Yeung <alan@creativebits.com.au>
2017-02-03 16:36:21 +0000

    Structural updates to decoration public viewing page

--- resources/views/public/decoration.blade.php
+++ resources/views/public/decoration.blade.php
@@ -1,33 +1,53 @@
 {{-- Individual decoration view public page --}}
 @extends('layouts.template')
 
+@section('title', 'View Decoration Info')
+
 @section('heading')
 <h4>Decoration info</h4>
 @endsection
 
 @section('content')
 <div class="row">
-    <div class="col-md-9 col-sm-6">
+    <div class="col-sm-6">
         <h1>{{ $dec->name }}</h1>
-        <h3>Tier {{ $dec->tier }} award</h3>
+        <h3>Award Tier <span class="label label-info">{{ $dec->tier }}</span></h3>
+
         <p>{{ $dec->desc  }}</p>
     </div>
-    <div class="col-md-3 col-sm-6">
-        <img src="{{ $decBadgeUrl }}" alt="{{ $dec->shortcode }}" title="{{ $dec->desc }}">
-        
+    <div class="col-sm-6">
+        <div class="well">
+            <img src="{{ $decBadgeUrl }}" alt="{{ $dec->shortcode }}" title="{{ $dec->desc }}">        
+        </div>
+        <table class="table table-striped">
+            <tbody>
+                <tr>
+                    <td>Precedence within Tier <span class="label label-info">{{ $dec->tier }}</label></td>
+                    <td>{{ $dec->precedence or 'N/A' }}</td>
+                </tr>
+                <tr>
+                    <td>Authorised by</td>
+                    <td>{{ $dec->authorized_by or 'N/A' }}</td>
+                </tr>
+                <tr>
+                    <td>Shortcode</td>
+                    <td>{{ $dec->shortcode or 'N/A' }}</td>
+                </tr>
+                <tr>
+                    <td>Minimum service period</td>
+                    <td>{{ $dec->service_period_months or 'N/A' }} months</td>
+                </tr>
+                <tr>
+                    <td>Date commenced</td>
+                    <td>{{ $dec->date_commence or 'N/A' }}</td>
+                </tr>
+                <tr>
+                    <td>Date concluded</td>
+                    <td>{{ $dec->date_conclude or 'N/A' }}</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 
 </div>
-<table class="table table-striped">
-    <tbody>
-        <tr>
-            <td>Precedence within Tier {{ $dec->tier }}</td>
-            <td>{{ $dec->precedence or 'Nope' }}</td>
-        </tr>
-        <tr>
-            <td>Authorised by</td>
-            <td>{{ $dec->authorized_by }}</td>
-        </tr>
-    </tbody>
-</table>
 @endsection
