45e106f3a46696250845c1a10f51f95d1616868c
Alan Yeung <alan@creativebits.com.au>
2017-02-19 14:04:24 +0000

    Shipped a cute little 'show more' toggle

--- resources/views/public/decoration-list.blade.php
+++ resources/views/public/decoration-list.blade.php
@@ -22,6 +22,7 @@
                     <col style="width: 120px;">
                     <col>
                     <col style="width: 120px;">
+                    <col style="width: 60px;">
                 </colgroup>
                 <thead>
                     <tr>
@@ -29,20 +30,27 @@
                         <th>Shortcode</th>
                         <th>Name</th>
                         <th>Eligible from</th>
+                        <th></th>
                     </tr>
                 </thead>
                 <tbody>
                 @foreach ($decorationTier->decorations as $dec)
-                    <tr onclick="openDecorationDetails('{{ route("public::decoration-details", [ "shortcode" => $dec->shortcode ]) }}')">
+                    <tr>
                         <td>
                             <span class="fl-dec-badge smaller"><img src="{{ $dec->badgeUrl }}" alt="{{ $dec->shortcode }}" title="{{ $dec->desc }}"></span>
                         </td>
                         <td><code>{{ $dec->shortcode }}</code></td>
-                        <td>{{ $dec->name }}</td>
+                        <td>
+                            {{ $dec->name }} 
+                            @if (count($dec->related)) &rsaquo; <a href="#" class="subdec-toggle" data-subdec-visible="false" onclick="toggleVisible({{ $dec->dec_id }}, this)">show more</a> @endif
+                        </td>
                         <td>{{ date('M Y', strtotime($dec->date_commence)) }}</td>
+                        <td><a class="btn btn-default btn-xs" href="{{ route('public::decoration-details', [ 'shortcode' => $dec->shortcode ]) }}" onclick="openDecorationDetails('{{ route("public::decoration-details", [ "shortcode" => $dec->shortcode ]) }}'); return false;">
+                            <span class="glyphicon glyphicon-new-window"></span>
+                        </a></td>
                     </tr>
                     @foreach ($dec->related as $related)
-                    <tr onclick="openDecorationDetails('{{ route("public::decoration-details", [ "shortcode" => $dec->shortcode ]) }}')">
+                    <tr class="info subdec-row" data-dec-parent="{{ $dec->dec_id }}" style="display: none;">
                         <td>
                             <span class="text-muted">&#8618;</span>
                             <span class="fl-dec-badge smaller"><img src="{{ $dec->badgeUrl }}" alt="{{ $dec->shortcode }}" title="{{ $dec->desc }}"></span>
@@ -50,6 +58,9 @@
                         <td><code>{{ $dec->shortcode }}</code></td>
                         <td>{{ $dec->name }}</td>
                         <td>{{ date('M Y', strtotime($dec->date_commence)) }}</td>
+                        <td><a class="btn btn-default btn-xs" href="{{ route('public::decoration-details', [ 'shortcode' => $dec->shortcode ]) }}" onclick="openDecorationDetails('{{ route("public::decoration-details", [ "shortcode" => $dec->shortcode ]) }}'); return false;">
+                            <span class="glyphicon glyphicon-new-window"></span>
+                        </a></td>
                     </tr>
                     @endforeach
                 @endforeach
@@ -64,6 +75,16 @@
 
 @push('scripts')
 <script>
+function toggleVisible(decorationId, toggleElem, cssDisplayValue){
+    cssDisplayValue = cssDisplayValue || 'table-row';
+    var currentlyVisible = toggleElem.getAttribute('data-subdec-visible') === 'true';
+    var rows = document.querySelectorAll('.subdec-row[data-dec-parent=\''+decorationId+'\']');
+    for (var i = 0; i < rows.length; i++){
+        rows[i].style.display = currentlyVisible ? 'none' : cssDisplayValue;
+    }
+    toggleElem.setAttribute('data-subdec-visible', currentlyVisible ? 'false' : 'true');
+    return false;
+}
 function openDecorationDetails(decorationDetailsUrl){
     window.open(decorationDetailsUrl, 'FlaresDecorationDescription', 'width=800, height=600'); 
     return false;
